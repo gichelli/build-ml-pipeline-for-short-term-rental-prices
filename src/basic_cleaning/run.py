19f2f18b68a83cafa4373405a21726322606eaa9
gichelli <ggonza18@masonlive.gmu.edu>
2023-08-16 22:09:51 -0400

    fix dropping outliers

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,12 +28,14 @@ def go(args):
     df = pd.read_csv(artifact_local_path)
 
     logger.info("Drop outliers")
-    # idx = df["price"].between(args.min_price, args.max_price)
-    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    idx = df["price"].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
     logger.info("Convert last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info("Save results in csv file")
     df.to_csv("clean_sample.csv", index=False)
