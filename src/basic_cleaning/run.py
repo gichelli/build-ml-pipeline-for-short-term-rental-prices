3bffda2a8b7db6726191799adf54eb018579e4f5
gichelli <ggonza18@masonlive.gmu.edu>
2023-08-16 21:10:40 -0400

    update dataframe longitud and latitude values

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,7 +28,8 @@ def go(args):
     df = pd.read_csv(artifact_local_path)
 
     logger.info("Drop outliers")
-    idx = df["price"].between(args.min_price, args.max_price)
+    # idx = df["price"].between(args.min_price, args.max_price)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
     logger.info("Convert last_review to datetime")
